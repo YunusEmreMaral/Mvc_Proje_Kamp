48f1c09f49b15bb5f630e538a93cd25f3a6e0c16
Yunus Emre Maral <108576886+ImGozluklu@users.noreply.github.com>
2024-01-25 01:10:52 +0300

    MvcProjeKamp2

---
+++ MvcProjeKamp2/DataAccessLayer/Abstract/ICategoryDal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using EntityLayer.Concrete;
+
+namespace DataAccessLayer.Abstract
+{
+   public interface ICategoryDal:IRepository<Category>
+    {
+        
+    }
+}
